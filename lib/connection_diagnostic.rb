7437089a2d39e7300ce8a3030e6003780f479bc9
Oscar <oscarbc@me.com>
2014-07-30 14:40:31 +0200

    message

--- lib/connection_diagnostic.rb
+++ lib/connection_diagnostic.rb
@@ -1,11 +1,43 @@
 require 'client'
+require 'pry'
 
 class ConnectionDiagnostic
   # perform_diagnostic
-  # disconnects the client
-  # tries to connect the client 3 times
-  # if unsuccessful raise Error
-  # if successful sends 'AT#UD' to client (diagnostic message)
-  #   and receives from the client the diagnostic info
-  #   this info should be accesible on the attribute info
+ 	 # disconnects the client
+  	# tries to connect the client 3 times
+  	# if unsuccessful raise Error
+  	# if successful sends 'AT#UD' to client (diagnostic message)
+  	#   and receives from the client the diagnostic info
+  	#   this info should be accesible on the attribute info
+
+
+
+  def initialize(client)
+  	@counter = 0 
+  	@client = client
+  end
+
+  def perform_diagnostic
+  	# disconects_client
+  	connects_client
+  end
+  
+  def disconects_client
+  	@client.disconnect
+  end
+
+  def connects_client
+  	answer = false
+  	if @client.connect
+			'AT#UD'
+		else 
+			@counter += 1
+			if @counter >= 2
+				raise "Error"
+			end
+			answer
+		end
+  end
+
+  connects_client rescue "Error Raised"
 end
--- spec/connection_diagnostic_spec.rb
+++ spec/connection_diagnostic_spec.rb
@@ -2,5 +2,40 @@ require 'spec_helper'
 
 describe ConnectionDiagnostic do
 
+  # perform_diagnostic
+  # disconnects the client
+  # tries to connect the client 3 times
+  # if unsuccessful raise Error
+  # if successful sends 'AT#UD' to client (diagnostic message)
+  #   and receives from the client the diagnostic info
+  #   this info should be accesible on the attribute info
+
+  it "disconects the client" do
+    client = Client.new
+    expect(ConnectionDiagnostic.new(client).disconects_client).to eq(false) 
+  end
+
+  it "tries to connect the client 1 time and fails" do
+    @client = double("client")
+    @client.stub(:connect).and_return(false)
+    @client.should_receive(:connect).and_return (false) 
+    expect(ConnectionDiagnostic.new(@client).perform_diagnostic).to eq(false)  
+  end
+
+  it "tries to connect the client 1 time and get successful" do
+    @client = double("client")
+    @client.stub(:connect).and_return(true)
+    expect(ConnectionDiagnostic.new(@client).perform_diagnostic).to eq('AT#UD')  
+  end
+
+  it "tries to connect three times and fails" do
+    @client = double("client")
+    @client.stub(:connect).and_return(false)
+    cd = ConnectionDiagnostic.new(@client)
+    cd.perform_diagnostic
+    cd.perform_diagnostic
+    expect(cd.perform_diagnostic).to raise_error("Error")
+    # expect(cd.perform_diagnostic).to eq("Error Raised")
+  end
 
 end
